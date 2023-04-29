2190eb4ff843bd35aa43b9a69a7bc9631b318855
alextanhongpin <alextan220990@gmail.com>
2023-04-29 23:51:27 +0800

    chore: add fastapi test

---
+++ tests/test_api.py
@@ -0,0 +1,9 @@
+def test_api_read_users(api):
+    response = api.get("/users")
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_api_read_user_bad_id(api):
+    response = api.get("/users/1")
+    assert response.status_code == 422
